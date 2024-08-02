2eaeac0af395f6cb30c7512b203fcb56fbd40f26
Dennis van Es <dpvanes67@gmail.com>
2024-08-02 08:38:20 +0200

    refactor: Update PUP.lua with additional equipment for CureBot set

--- PUP.lua
+++ PUP.lua
@@ -94,9 +94,16 @@
 
     sets.tp["CureBot"] = {
         head={ name="Naga Somen", augments={'Pet: MP+75','Automaton: "Cure" potency +3%','Automaton: "Fast Cast"+2',}},
+        neck="Ishtar's Collar",
         left_ear="Cessance Earring",
         right_ear="Mache Earring",
+        body="Naga Samue",
         hands={ name="Naga Tekko", augments={'Pet: MP+80','Automaton: "Cure" potency +4%','Automaton: "Fast Cast"+3',}},
+        left_ring="Tali'ah Ring",
+        right_ring="Petrov Ring",
+        back="Visucius's Mantle",
+        waist="Kuku Stone",
+        legs="Tali'ah Seraweels +1",
         feet={ name="Naga Kyahan", augments={'Pet: MP+80','Automaton: "Cure" potency +4%','Automaton: "Fast Cast"+3',}},
     }
 
@@ -139,6 +146,7 @@
         neck="Buffoon's Collar",
         body="Cirque Farsetto +1",
         hands = "Puppetry Dastanas",
+        back="Dispersal Mantle",
     }
     sets.ja['Repair'] = {
         feet = "Puppetry Babouches",
